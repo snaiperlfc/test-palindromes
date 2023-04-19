af447b1f040a90c7ecc04885cbfe62c0cfb1e809
snaiperlfc <snaiperlfc@ya.ru>
2023-04-19 19:01:20 +0300

    added tests

---
+++ src/main/java/org/test/LongestPalindromicSubstring.java
@@ -0,0 +1,10 @@
+package org.test;
+
+public class LongestPalindromicSubstring {
+
+    public String longestPalindrome(String s) {
+        //write this code
+
+        return null;
+    }
+}
--- src/test/java/org/test/PalindromesTest.java
+++ src/test/java/org/test/PalindromesTest.java
@@ -1,23 +1,55 @@
 package org.test;
 
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
-
-import static org.junit.jupiter.api.Assertions.*;
 
 class PalindromesTest {
 
     @Test
-    void longestPalindrome() {
+    void testExample1() {
+        String s = "babad";
+        String expected = "aba";
+        String actual = new LongestPalindromicSubstring().longestPalindrome(s);
+        Assertions.assertEquals(expected, actual);
+    }
 
+    @Test
+    void testExample2() {
+        String s = "cbbd";
+        String expected = "bb";
+        String actual = new LongestPalindromicSubstring().longestPalindrome(s);
+        Assertions.assertEquals(expected, actual);
+    }
 
-        String t1 = Palindromes.longestPalindrome("babad");
+    @Test
+    void testSingleCharacter() {
+        String s = "a";
+        String expected = "a";
+        String actual = new LongestPalindromicSubstring().longestPalindrome(s);
+        Assertions.assertEquals(expected, actual);
+    }
 
-        String t2 = Palindromes.longestPalindrome("cbbd");
+    @Test
+    void testAllSameCharacters() {
+        String s = "bbbbbbbbbb";
+        String expected = "bbbbbbbbbb";
+        String actual = new LongestPalindromicSubstring().longestPalindrome(s);
+        Assertions.assertEquals(expected, actual);
+    }
 
+    @Test
+    void testNoPalindromicSubstring() {
+        String s = "abcdefg";
+        String expected = "g";
+        String actual = new LongestPalindromicSubstring().longestPalindrome(s);
+        Assertions.assertEquals(expected, actual);
+    }
 
-        assertEquals("aba", t1);
-
-        assertEquals("bb", t2);
-
+    @Test
+    void testEmptyString() {
+        String s = "";
+        String expected = "";
+        String actual = new LongestPalindromicSubstring().longestPalindrome(s);
+        Assertions.assertEquals(expected, actual);
     }
 }
